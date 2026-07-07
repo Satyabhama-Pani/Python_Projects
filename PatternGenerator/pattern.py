ef42a054c73487b97750db8e386d653cf50f38a4
Satyabhama Pani <panisatyabhama857@gmail.com>
2026-07-07 22:34:58 +0530

    Pattern File added

---
+++ PatternGenerator/pattern.py
@@ -0,0 +1,17 @@
+# welcome note
+def menu():
+    print("\n")
+    print("*"*40)
+    print(f"{"\u2728 Pattern Generator\u2728":^40}")
+    print("*"*40)
+    print("\n")
+    print(f"""Options are: 
+    1. Square Pattern
+    2. left Triangle Pattern
+    3. Right Triangle Pattern
+    4. Inverted Triangle
+    5. Pyramid Pattern
+    6. Inverted Pyramid Pattern
+    7. Diamond Pattern
+    8. Hollow Square
+    \n""")
