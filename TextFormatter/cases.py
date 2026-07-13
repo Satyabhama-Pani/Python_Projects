3c0762b1c603f92b388fb0565a42366a1120a8bf
Satyabhama Pani <panisatyabhama857@gmail.com>
2026-07-13 21:34:07 +0530

    Added Text Formatter in Python Projects

---
+++ TextFormatter/cases.py
@@ -0,0 +1,16 @@
+# Main menu
+def menu():
+    print("\n"+ "*"*40)
+    print("Text Formatter")
+    print("*"*40 + "\n")
+    print(f'''Cases options are:
+          1. UPPERCASE
+          2. lowercase
+          3. Title Case
+          4. Sentence case
+          5. Swap case(lower to upper and vice-versa)
+          6. camelCase
+          7. PascalCase
+          8. snake_case
+          9. kebab-case
+          \n''')
