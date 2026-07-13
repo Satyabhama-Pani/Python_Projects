c9ae1dcfe523530de1204980b29139ca2acdbddf
Satyabhama Pani <panisatyabhama857@gmail.com>
2026-07-13 21:48:36 +0530

    Added Camel and Pascal case

--- TextFormatter/cases.py
+++ TextFormatter/cases.py
@@ -15,13 +15,26 @@ def menu():
           9. kebab-case
           \n''')
 # All case function
+# Upper case
 def uppercase(text):
     return text.upper()
+# Lower case
 def lowercase(text):
     return text.lower()
+# Title case
 def titlecase(text):
     return text.title()
+# Sentence case
 def sentencecase(text):
     return text.capitalize()
+# Swap case
 def swapcase(text):
     return text.swapcase()
+# Camel case
+def camelcase(text):
+    words = text.split()
+    return words[0].lower() + "".join(word.capitalize() for word in words[1:])
+# Pascal case
+def pascalcase(text):
+    words = text.split()
+    return "".join(word.capitalize() for word in words)
