48c78ecacbbad9f62cad16ce46343e8dd2b693c7
Satyabhama Pani <panisatyabhama857@gmail.com>
2026-07-17 21:35:11 +0530

    New Folder Added - KBC(kbc_quiz)

---
+++ KBC/kbc_quiz.py
@@ -0,0 +1,39 @@
+# Questions  dictionary
+questions = {
+    "In Hindi mythology, who is the elder brother Of lord Krishna": {
+        "A": "Balarama",
+        "B": "Lakshmana",
+        "C": "Bharata",
+        "D": "Dhritrashtra",
+        "answer": "A"
+    },
+    "Which of the folloowing holy river flows directly through the ancient city of Varansi?": {
+        "A": "Yamuna",
+        "B": "Ganga",
+        "C": "Bhramhaputra",
+        "D": "Narmada",
+        "answer": "B"
+    },
+    "Who was the founder of Maurya Empire?": {
+        "A": "Bindusara",
+        "B": "Samudragupta",
+        "C": "Chandragupta Maurya",
+        "D": "Ashoka The Great",
+        "answer": "C"
+    },
+    "Which planet is widely known as Red Planet?": {
+        "A": "Venus",
+        "B": "Mars",
+        "C": "Mercury",
+        "D": "Jupiter",
+        "answer": "B"
+    },
+    "Which city is known as Marble city?": {
+        "A": "Agra",
+        "B": "Jaiur",
+        "C": "Udaipur",
+        "D": "Jabalpur",
+        "answer": "D"
+    }
+}
+
