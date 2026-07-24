bd540acfa7283176d2507aedebbfd8d37f90450d
Satyabhama Pani <panisatyabhama857@gmail.com>
2026-07-24 22:39:24 +0530

    Added school class

--- StudentManagement/student_manage.py
+++ StudentManagement/student_manage.py
@@ -13,4 +13,19 @@ class Student:
         print(f"Roll No.: {self.roll}")
         print(f"Marks: {self.marks}")
         print(f"College: {self.college}")
-        
+class School:
+    students = []
+    def menu(self):
+        print("\n"+ "*"*40)
+        print(f"{"🎓Student Management System":^40}")
+        print("*"*40)
+        print(f'''
+            1. Add Student
+            2. View Students
+            3. Search Student
+            4. Update Marks
+            5. Delete Student
+            6. View Class Wise Student 
+            7. Show Class teacher
+            8. Exit
+            ''')       
