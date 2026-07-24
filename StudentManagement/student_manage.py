c7d860c2910a771e4ed117a9f197208e22b73fc0
Satyabhama Pani <panisatyabhama857@gmail.com>
2026-07-24 22:22:12 +0530

    Added new folder

---
+++ StudentManagement/student_manage.py
@@ -0,0 +1,15 @@
+class Student:
+    college = "United Institute Of Management"
+    def __init__(self,name,roll,marks,classes):
+        self.name = name
+        self.roll = roll
+        self.marks = marks
+        self.classes = classes 
+    def display(self):
+        print("\n"+ "*"*30)
+        print(f"{"Student Details":^30}")
+        print("*"*30)
+        print(f"Name: {self.name}")
+        print(f"Roll No.: {self.roll}")
+        print(f"Marks: {self.marks}")
+        print(f"College: {self.college}")
